c7419f00e5c1bb03cd62ef0bc54e32435c6ea0cf
lwebert <lauren@weberts.org>
2025-01-12 22:06:27 -0600

    Base code + schema.sql

---
+++ develop/db/seeds.sql
@@ -0,0 +1,13 @@
+-- transaction?
+
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department)
+VALUES ('Sales Lead, 5000, 1'),
+('Software Engineer, 10000, 2'),
+('');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ();
+
